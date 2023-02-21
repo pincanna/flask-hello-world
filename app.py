b8457fc777f601fb4c1032ccc04a937ba2e65a01
Zach Nusbaum <zach@zdnmail.com>
2023-02-20 21:48:19 -0800

    Add UUID Endpoint

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask
+from uuid import uuid4
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/uuid4')
+def get_uuid4():
+    return {'value': str(uuid4())}
